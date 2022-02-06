3ba18671a681e81e508d52908b39fa1c8b6b995f
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-02-06 16:26:54 +0530

    redux setup is done

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 
 import todos from "./todos";
-
+import productReducer from "./productReducer";
 export default combineReducers({
   todos,
+  allproducts: productReducer,
 });
+
+// created allproducts as alias, can be use as simple too
