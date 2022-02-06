9b91ae68f5a74265951045e122fea421e6eacee1
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-02-06 18:11:39 +0530

    detail page is also working now

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 
 import todos from "./todos";
-import productReducer from "./productReducer";
+import { productReducer, selectedProductReducer } from "./productReducer";
 export default combineReducers({
   todos,
   allProducts: productReducer,
+  product: selectedProductReducer,
 });
 
 // created allproducts as alias, can be use as simple too
