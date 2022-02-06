34521bbd2d08a2772206f370e05f59dfc0e3d0cb
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-02-06 15:51:15 +0530

    product list with react redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { Container, Heading } from "@chakra-ui/react";
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
-import { VisibiltyFilter } from "./components/VisibiltyFilter";
 function App() {
   return (
-    <Container maxWidth="container.sm">
-      <Heading my="4">Todo List</Heading>
-      <AddTodo></AddTodo>
-      <TodoList></TodoList>
-      <VisibiltyFilter></VisibiltyFilter>
-    </Container>
+    <div className="App">
+      <h1>Header</h1>
+    </div>
   );
 }
 
