3b95dca88dc6f24a6e75a37705db1607398f32fe
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-12-27 18:28:02 +0530

    react-pokemon-list
    react-pokemon-list

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, { Component } from "react";
-import "./App.css";
-import Cart from "./components/Cart";
-import Filter from "./components/Filter";
-import Products from "./components/Products";
-import data from "./data.json";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import './App.css';
+import {Switch, Route, NavLink, Redirect} from "react-router-dom";
+import PokemonList from "./containers/PokemonList";
+import Pokemon from "./containers/Pokemon";
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="grid-container">
-          <header>
-            <a href="/">React shopping cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter></Filter>
-                <Products></Products>
-              </div>
-              <div className="sidebar">{<Cart></Cart>}</div>
-            </div>
-          </main>
-          <footer>All right is researved.</footer>
-        </div>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <NavLink to={"/"}>Search</NavLink>
+      </nav>
+      <Switch>
+        <Route path={"/"} exact component={PokemonList} />
+        <Route path={"/pokemon/:pokemon"} exact component={Pokemon} />
+        <Redirect to={"/"} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,18 +1,9 @@
-import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {applyMiddleware} from "redux";
 import thunk from "redux-thunk";
-import { cartReducer } from "./reducers/cartReducer";
-import { productsReducer } from "./reducers/productReducer";
-import { orderReducer } from "./reducers/orderReducer";
+import RootReducer from "./reducers/RootReducer";
 
-const initialState = {};
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  combineReducers({
-    products: productsReducer,
-    cart: cartReducer,
-    order: orderReducer,
-  }),
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
-);
-export default store;
+const Store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default Store
