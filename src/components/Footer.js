bafa934e0cc452238edeb399310c8d3cf9e30516
Anil Kumar <anilkumar.dhiman1@gmail.com>
2023-08-07 19:00:42 +0530

    few other files added

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { socialLink } from "../data";
+
+function Footer() {
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        <li>
+          <a href="#home" className="footer-link">
+            home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="footer-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="footer-link">
+            services
+          </a>
+        </li>
+        <li>
+          <a href="#featured" className="footer-link">
+            featured
+          </a>
+        </li>
+      </ul>
+      <ul className="footer-icons">
+        {socialLink.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} target="_blank" className="footer-icon">
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"></span> all rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const pageLinks = [
+  { id: 1, href: "#home", title: "home" },
+  { id: 2, href: "#about", title: "about" },
+  { id: 3, href: "#services", title: "services" },
+  { id: 4, href: "#tours", title: "tours" },
+];
+
+export const socialLink = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.fb.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+];
