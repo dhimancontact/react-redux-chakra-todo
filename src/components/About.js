5ce51dae7e652e2c18a29aacba7ab81468c71ab9
Anil Kumar <anilkumar.dhiman1@gmail.com>
2023-08-08 17:22:30 +0530

    ignore error by netlify

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ function About() {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#aboutus" className="btn">
+          <a href="#" className="btn">
             read more
           </a>
         </article>
