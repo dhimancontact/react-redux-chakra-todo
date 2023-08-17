27399c8735cc4857f6bfb447da536bfe6d9ff9e6
Anil Kumar <anilkumar.dhiman1@gmail.com>
2023-08-17 18:58:01 +0530

    route set

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import AboutPage from "./AboutPage";
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProductPage from "./SingleProductPage";
+import CartPage from "./CartPage";
+import ErrorPage from "./ErrorPage";
+import CheckoutPage from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  AboutPage,
+  Products,
+  SingleProductPage,
+  CartPage,
+  ErrorPage,
+  CheckoutPage,
+  PrivateRoute,
+};
