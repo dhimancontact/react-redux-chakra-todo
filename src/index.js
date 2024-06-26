91295b6bd6182efc5869864109c43189ad1ce919
Anil Kumar <1304712+anildhiman03@users.noreply.github.com>
2024-06-26 18:07:40 +0000

    remove unwanted code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Provider } from "react-redux";
 import store from "./redux/store";
 ReactDOM.render(
-  <Provider store={store}>
-    <ChakraProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChakraProvider>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
